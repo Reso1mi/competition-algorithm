45a8b7ad31ce9b229b91d87e8b336717181127ff
lgw <privateli@qq.com>
2020-11-09 00:02:46 +0800

    fix

--- 动态规划/数字三角形模型/AcWing1027_方格取数.java
+++ 动态规划/数字三角形模型/AcWing1027_方格取数.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*; 
-public class AcWing1027_方格取数 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
