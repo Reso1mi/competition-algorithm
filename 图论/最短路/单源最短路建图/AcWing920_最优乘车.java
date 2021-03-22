7cab8a24c910306cefe41fa7911a3de126bd5ff1
lgw <privateli@qq.com>
2021-03-22 22:16:50 +0800

    AcWing920.最优乘车

--- 图论/最短路/单源最短路建图/AcWing920_最优乘车.java
+++ 图论/最短路/单源最短路建图/AcWing920_最优乘车.java
@@ -5,14 +5,7 @@ class Main {
 
     static int INF = 0x3f3f3f3f;
     static int N, M;
-    static int[] e, ne, w, h;
-    static int idx;
-    static int MAX = 100010;
-    //a->b
-    public static void add(int a, int b, int c){
-        e[idx] = b; ne[idx] = h[a];
-        w[idx] = c; h[a] = idx++;
-    }
+    static int[][] w;
 
     public static void main(String... args) throws Exception {
         PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
@@ -20,12 +13,17 @@ class Main {
         // BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("./input.txt")));
         int[] in = read(br);
         M = in[0]; N = in[1];
-        e = new int[MAX]; ne = new int[MAX]; w = new int[MAX];
-        h = new int[N+1]; Arrays.fill(h, -1);
-        for (int i = 0; i < M; i++) {
+        w = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(w[i], INF);
+        }
+        //M*N*N
+        for (int i = 1; i <= M; i++) {
             int[] t = read(br);
-            for (int j = 0; j < t.length-1; j++) {
-
+            for (int j = 0; j < t.length; j++) {
+                for (int k = j+1; k < t.length; k++) {
+                    w[t[j]][t[k]] = 1;
+                }
             }
         }
         int[] dis = new int[N+1];
@@ -37,15 +35,15 @@ class Main {
             int i = pq.poll();
             if (vis[i]) continue;
             vis[i] = true;
-            for (int j = h[i]; j != -1; j = ne[j]) {
-                int k = e[j];
-                if (dis[k] > dis[i] + w[j]) {
-                    dis[k] = dis[i] + w[j];
-                    pq.add(k);
+            for (int j = 1; j <= N; j++) {
+                if (w[i][j] == INF) continue;
+                if (dis[j] > dis[i] + w[i][j]) {
+                    dis[j] = dis[i] + w[i][j];
+                    pq.add(j);
                 }
             }
         }
-        out.println(dis[N]==INF ? "No" : dis[N]);
+        out.println(dis[N]==INF ? "NO" : dis[N]-1);
         out.flush();
     }
 
