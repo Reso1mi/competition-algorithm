a3ba858eb1cc2da0fba1875f354c134bf36bfe7a
Resolmi <privateli@qq.com>
2022-03-02 00:55:58 +0800

    LeetCode2187. 完成旅途的最少时间（二分答案）

--- IO模板/Golang/GoMain.go
+++ IO模板/Golang/GoMain.go
@@ -20,6 +20,33 @@ func main() {
 	writer.Flush()
 }
 
+type Node struct {
+	idx int
+	val int
+}
+
+type NodeHeap []*Node
+
+func (h NodeHeap) Len() int { return len(h) }
+
+// 小顶堆
+func (h NodeHeap) Less(i, j int) bool { return h[i].val < h[j].val }
+func (h NodeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *NodeHeap) Push(x interface{}) {
+	// Push 和 Pop 使用 pointer receiver 作为参数，
+	// 因为它们不仅会对切片的内容进行调整，还会修改切片的长度。
+	*h = append(*h, x.(*Node))
+}
+
+func (h *NodeHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
 func ReadLine(reader *bufio.Reader) string {
 	line, _ := reader.ReadString('\n')
 	return strings.TrimRight(line, "\n")
