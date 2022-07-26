27ab3d6fc1f051f2253450634ace82abbb807acd
resolmi <privateli@qq.com>
2022-07-26 21:50:42 +0800

    upd: 构造题

--- 构造/cf1000/CF_1433_C.java
+++ 构造/cf1000/CF_1433_C.java
@@ -1,8 +1,10 @@
 import java.util.*;
 import java.io.*;
-// https://codeforces.com/problemset/problem/1401/A
+
+// https://codeforces.com/problemset/problem/1433/C
+
 // AtCoder/AcWing 提交上面部分即可，CF需要将JavaMain移到上面然后提交
-public class CF_1401_A {
+public class CF_1433_C {
     public static void main(String[] args) throws Exception{
         // 输入重定向，通过jvm参数判断环境
         if (args.length > 0 && "Resolmi_DEBUG".equals(args[0])) {
@@ -12,27 +14,36 @@ public class CF_1401_A {
     }
 }
 
+
 class Main {
 
-    // k > n: OB-AB = OA = k
-    // k < n: OB+BA=OA OB-BA=k 2OB = k + OA
-    //                 BA-OB=k 2BA = k + OA
+    // 5 3 4 4 5
+    // 4 4 3 4 4
+    // 2 3 1 1 2 4
     public static void main(String[] args) throws Exception {
         PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = read(br)[0];
         while (T-- > 0) {
-            int[] in = read(br);
-            int n = in[0], k = in[1];
-            if (k >= n) {
-                out.println(k-n);
-            } else {
-                if (((k+n)&1) == 0) {
-                    out.println(0);
-                } else {
-                    out.println(1);
+            read(br);
+            var t = read(br);
+            int max = 0;
+            for (int i = 0; i < t.length; i++) {
+                max = Math.max(max, t[i]);
+            }
+            int res = -1;
+            for (int i = 0; i < t.length; i++) {
+                if (t[i] != max) continue;
+                if (i > 0 && t[i] > t[i-1]) {
+                    res = i+1;
+                    break;
+                }
+                if (i < t.length-1 && t[i] > t[i+1]) {
+                    res = i+1;
+                    break;
                 }
             }
+            out.println(res);
         }
         out.flush();
     }
